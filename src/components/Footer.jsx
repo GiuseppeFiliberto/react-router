e733248d3f31509dba8fea9d3b6c7f53751f91ca
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-04-01 17:29:16 +0200

    Add Footer component with basic layout and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+    return (
+        <>
+            <footer className='py-5 bg-light mt-5'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            Logo
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sint consectetur rerum voluptatibus, architecto tempore ipsa nemo ducimus officiis, itaque a similique incidunt iure. Ex velit id voluptatum magnam facere.</p>
+                        </div>
+                        <div className="col">
+                            <h3>Lorem</h3>
+                            <ul className="list-unstyled">
+                                <li>lorem</li>
+                                <li>lorem</li>
+                                <li>lorem</li>
+                                <li>lorem</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
